2ddcb30fbdb6ab6e6e92f145d2d9e529adfd9d1f
Galantii <127208542+Galantii@users.noreply.github.com>
2023-03-07 11:26:40 -0300

    Add files via upload

---
+++ Aula 5/banco/src/ContaCorrente.java
@@ -0,0 +1,43 @@
+public final class ContaCorrente extends Conta {
+
+	private double saldoInvestimento;
+
+	public ContaCorrente(Cliente cliente, long numeroConta) {
+		super(cliente, numeroConta);
+	}
+
+	// design pattern Strategy
+	public void investir(Produto produto, double valor) throws SaldoInsuficiente {
+		boolean teste = true;
+		teste = this.sacar(valor);
+		if (teste == true) {
+			this.saldoInvestimento += produto.investir(valor);
+		}
+	}
+
+	public void resgatar(double valor) throws SaldoInsuficiente {
+		if (this.saldoInvestimento >= valor) {
+			this.saldoInvestimento -= valor;
+			this.depositar(valor);
+		} else {
+			throw new SaldoInsuficiente("Saldo de investimento insuficiente");
+		}
+	}
+
+	@Override
+	public void exibirSaldo() {
+		double saldoTotal = this.saldo + this.saldoInvestimento;
+		System.out.println("Cliente: " + this.cliente.getNome());
+		System.out.println("Saldo conta corrente: R$ " + this.saldo);
+		System.out.println("Saldo investimento: R$ " + this.saldoInvestimento);
+		System.out.println("Saldo total: R$ " + saldoTotal + "\n");
+		System.out.println("Data de abertura da conta: " + this.dataAbertura);
+	}
+
+	public void investimento(double valor, Produto produto) throws SaldoInsuficiente {
+		boolean teste = this.sacar(valor);
+		if (teste == true) {
+			saldoInvestimento += produto.investir(valor);
+		}
+	}
+}
---
+++ Aula 5/banco/src/Fundos.java
@@ -0,0 +1,10 @@
+public class Fundos implements Produto {
+
+	@Override
+	public double investir(double valor) {
+		return valor * 1.05;
+	}
+
+	
+
+}
